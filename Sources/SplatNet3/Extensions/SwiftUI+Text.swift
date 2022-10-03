edf27759c9b4f89fd33f4329b8b08780051d569f
tkgstrator <nasawake.am@gmail.com>
2022-10-03 23:13:30 +0900

    update: add localized enum

---
+++ Sources/SplatNet3/Extensions/SwiftUI+Text.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI+Text.swift
+//  
+//
+//  Created by devonly on 2022/10/03.
+//
+
+
+import Foundation
+import SwiftUI
+
+public extension Text {
+    init(bundle: LocalizedText) {
+        self.init(NSLocalizedString(bundle.rawValue, bundle: .main, comment: ""))
+    }
+}
